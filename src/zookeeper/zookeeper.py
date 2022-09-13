20c41b2092a87021f245047d1af0e0f842baf322
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2022-09-13 22:37:11 +0200

    stage 2 complete - just print an ASCII art

--- src/zookeeper/zookeeper.py
+++ src/zookeeper/zookeeper.py
@@ -1,8 +1,20 @@
-output = '''
-I love animals!
-Let's check on the animals...
-The deer looks fine.
-The bat looks happy.
-The lion looks healthy.
-'''
-print(output)
+camel = r"""Switching on the camera in the camel habitat...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \ 
+     |     \    _.-'             \ 
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Look at that! Our little camel is sunbathing!"""
+print(camel)  # r""" for raw string...
